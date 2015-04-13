f7b36a67b3b329a383d53dfa3d2c8c9ba59b5632
Enapiuz <enapiuz@gmail.com>
2015-04-14 00:47:28 +0300

    Проба Spider. Парсинг всего топа харба.

--- scrapingLearn02/grabber.py
+++ scrapingLearn02/grabber.py
@@ -1,9 +1,53 @@
 __author__ = 'enapiuz'
 
-
-from grab import Grab
-from pymongo.errors import DuplicateKeyError
+from grab.spider import Spider, Task
+from selection import XpathSelector
 from helpers import mongo as db
 
 
-g = Grab()
+class MegaParser(Spider):
+    max_page = 100
+    processed = 0
+
+    def task_generator(self):
+        for page_num in range(self.max_page):
+            yield Task('page', url='http://habrahabr.ru/all/page{number}'.format(number=page_num + 1))
+
+            self.processed += 1
+            print("Processed {count}%".format(count=round((self.processed/self.max_page)*100)))
+
+    def task_page(self, grab, task):
+        cp = grab.doc.select('//*[@id="nav-pages"]/li/em').text()
+        for article_url in grab.doc.select("//h1[@class='title']/a[@class='post_title']/@href"):
+            yield Task('article', url=article_url.text(), current_page=cp)
+
+    def task_article(self, grab, task):
+        page_header = grab.doc.select("//span[@class='post_title']").one(default=XpathSelector('No Header!')).text()
+        page_favs = grab.doc.select("//*[@class='favs_count']").one(default=XpathSelector('0')).text()
+        page_score = grab.doc.\
+            select("//div[@class='infopanel_wrapper']//div[contains(@class, 'mark')]/span[@class='score']").\
+            one(default=XpathSelector('0')).text()
+        page_comments_count = grab.doc.select("//*[@id='comments_count']").one(default=XpathSelector('0')).text()
+        page_author = grab.doc.select("//div[@class='author']/a").one(default=XpathSelector('No Author!')).text()
+        page_date = grab.doc.select("//div[@class='published']").one(default=XpathSelector('0')).text()
+
+        self.save_result(data={
+            'page': task.current_page,
+            'url': task.url,
+            'header': page_header,
+            'author': page_author,
+            'favorites': page_favs,
+            'score': page_score,
+            'comments': page_comments_count,
+            'date': page_date
+        })
+
+    # а нужен ли тут статический метод?
+    @staticmethod
+    def save_result(data):
+        db.save_entry(data)
+
+
+if __name__ == '__main__':
+    parser = MegaParser()
+    parser.run()
--- scrapingLearn02/helpers/mongo.py
+++ scrapingLearn02/helpers/mongo.py
@@ -1,8 +1,19 @@
 __author__ = 'enapiuz'
 
 import pymongo
+from pymongo.errors import DuplicateKeyError
 
 # TODO сделать из этого класс, конструктор которого принимает имя базы и, возможно, коллекцию для удобства
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.testgrab
+collection = db.test2collection
+collection.create_index("url", unique=True)
+
+
+# TODO нормально починить индекс
+def save_entry(entry):
+    try:
+        return collection.insert_one(entry).inserted_id
+    except DuplicateKeyError as e:
+        return -1
