af388ad942777e097e53f93d1c2050d470d4a6a9
Enapiuz <enapiuz@gmail.com>
2015-04-15 00:25:40 +0300

    Заготовка под парсер гугла

---
+++ scrapingLearn03/grabber.py
@@ -0,0 +1,37 @@
+__author__ = 'enapiuz'
+
+import sys
+import argparse
+import urllib.parse
+from grab.spider import Spider, Task
+from selection import XpathSelector
+from helpers import db
+
+
+# получаем поисковой запрос и количество просматриваемых страниц
+argParser = argparse.ArgumentParser()
+argParser.add_argument('-q', '--query')
+argParser.add_argument('-p', '--pages')
+args = argParser.parse_args()
+query = args.query or ""
+pages_limit = args.pages or 1
+
+if len(query) == 0:
+    sys.exit('Usage: (-q "query") [-p total_pages]')
+
+
+class GoogleParser(Spider):
+    def __init__(self, param_pages_limit, param_query):
+        self.param_pages_limit = param_pages_limit
+        self.param_query = param_query
+        super().__init__()
+
+    def task_generator(self):
+        yield Task('do_request', url="https://www.google.com/?" + urllib.parse.urlencode({'q': self.param_query}))
+
+    def task_do_request(self, grab, task):
+        print(task.url)
+
+
+parser = GoogleParser(pages_limit, query)
+parser.run()
